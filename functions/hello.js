542cf2cdeaca97db9bf91d210f3f78d9675b9307
moad.fethallah <moad.fethallah@student.ecp.fr>
2020-10-27 14:02:09 +0100

    update hello 2

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,6 @@
-export async function handler(event, context) {
+exports.handler = async function(event, context) {
     return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello World" }),
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello World" }),
     };
-}
+  };
